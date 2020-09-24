67ea11f7e45f2ef3f6a3cec10bd43388149cf0e6
Samuel Jo <jo.sunghak@gmail.com>
2020-09-24 11:49:50 -0400

    Updates

---
+++ precache-manifest.351510ee84e48e7074e116557be0d933.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa8c67ca11d30f78eac1599794447e4",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "f7bcfa76dff65790d8fa",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d0592409a7a420ab77d0",
+    "url": "/covid-tracker/static/css/main.fe34914e.chunk.css"
+  },
+  {
+    "revision": "f7bcfa76dff65790d8fa",
+    "url": "/covid-tracker/static/js/2.feb4c697.chunk.js"
+  },
+  {
+    "revision": "af5492f10234ec8ba29a624c4fe6fb11",
+    "url": "/covid-tracker/static/js/2.feb4c697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0592409a7a420ab77d0",
+    "url": "/covid-tracker/static/js/main.cc3eaa88.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  }
+]);
